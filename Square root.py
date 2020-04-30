fe023635acfd9b9d7e255d1b5d701398e9d2ab57
Declan <deciohara@gmail.com>
2020-04-30 13:15:21 +0100

    square root

--- Square root.py
+++ Square root.py
@@ -1,5 +1,10 @@
+#input asks the user to enter a positive number
+# square a*s is the calculation
+#print(f) allows us to add integers and outputs to printed strings in the form of curly brackets {}
 
 a = int(input("Please enter a positive number: "))
 
-print (a , end=' ')
+square = a*a
+
+print(f"The Square of {a} is {square}.")
 
