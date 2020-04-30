965bb7ae675bf9f1472ef863a3baf00736a3f3d8
Declan <deciohara@gmail.com>
2020-04-30 12:31:41 +0100

    settings

---
+++ Square root.py
@@ -0,0 +1,5 @@
+
+a = int(input("Please enter a positive number: "))
+
+print (a , end=' ')
+
---
+++ plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 1, 2, 3, 4)
+
+f = x**2
+
+fig = plt.figure()
+plt.show()
